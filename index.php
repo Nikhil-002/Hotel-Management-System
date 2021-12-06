468ebaf41c97a84d58a64361534d324c5d1464df
Pushp Jain <77234511+pushpjain3@users.noreply.github.com>
2021-12-07 01:57:07 +0530

    book room backend working

--- index.php
+++ index.php
@@ -1,16 +1,45 @@
 <?php
+$insert = false;
+$msg = "";
+if(isset($_POST['cust_id'])){
+    // Set connection variables
+    $server = "localhost";
+    $username = "root";
+    $password = "";
 
-  $username = "root";
-  $server = "localhost";
-  $password = "";
+    // Create a database connection
+    $con = mysqli_connect($server, $username, $password);
 
-  $con = mysqli_connect($server,$username, $password);
-
-  if(!$con){
+    // Check for connection success
+    if(!$con){
         die("connection to this database failed due to" . mysqli_connect_error());
     }
+    // echo "Success connecting to the db";
 
+    // Collect post variables
+    $cust_id = $_POST['cust_id'];
+    $trans_type = $_POST['trans_type'];
+    $trans_amount = $_POST['trans_amount'];
+   
+    
+    $sql = "INSERT INTO `hotel_management`.`transactions` (`trans_type`, `trans_amount`, `tarns_date`, `cust_id`) VALUES ( '$trans_type', '$trans_amount', current_timestamp(), '$cust_id');";
+    // echo $sql;
 
+    // Execute the query
+    if($con->query($sql) == true){
+        // echo "Successfully inserted";
+
+        // Flag for successful insertion
+        $insert = true;
+        $msg = "<p class='submitMsg'>Thanks for submitting your form. Customer Added</p>";
+    }
+    else{
+        echo "ERROR: $sql <br> $con->error";
+    }
+
+    // Close the database connection
+    $con->close();
+}
 ?>
 
 <!DOCTYPE html>
@@ -42,18 +71,22 @@
       </ul>
     </nav>
     <div class="container">
-      <form action="index.php">
+       <?php
+        if($insert == true){
+        echo $msg;
+        }
+    ?>
+      <form action="index.php" method="post">
         <h1><b>Book Room</b></h1>
-        <input type="int" placeholder="Enter Customer-Id " />
-        <input type="int" placeholder="Enter Reception-Id" />
-        <select class="dropdown" aria-label="Transaction Mode">
+        <input name="cust_id" id="cust_id"type="int" placeholder="Enter Customer-Id " />
+        <select name="trans_type" id="trans_type" class="dropdown" aria-label="Transaction Mode">
           <option selected>Transaction Mode</option>
           <option value="Net Banking">Net Banking</option>
           <option value="Credit Card">Credit Card</option>
           <option value="Debit Card">Debit Card</option>
           <option value="Cash">Cash</option>
         </select>
-        <input type="int" placeholder="Amount" />
+        <input name="trans_amount" id="trans_amount" type="int" placeholder="Amount" />
         <button class="btn">Submit</button>
       </form>
     </div>
