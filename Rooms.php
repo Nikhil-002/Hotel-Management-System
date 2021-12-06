bc0c114ffdc222edc68b410d083d5add1c7c0714
Nikhil-002 <93790466+Nikhil-002@users.noreply.github.com>
2021-12-07 02:18:47 +0530

    Update Rooms.php

--- Rooms.php
+++ Rooms.php
@@ -5,9 +5,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rooms</title>
-    lin
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+<nav class="navbar">
+      <a class="navbar-brand" href="#"> HOTEL MANAGEMENT</a>
+      <ul class="list-non-bullet nav-pills">
+        <li class="list-item-inline">
+          <a class="link link-active" href="./">Book Room</a>
+        </li>
+        <li class="list-item-inline">
+          <a class="link" href="./AddCustomer.php">Add Customer</a>
+        </li>
+        <li class="list-item-inline">
+          <a class="link" href="./Rooms.php">Rooms</a>
+        </li>
+        <li class="list-item-inline">
+          <a class="link" href="./Transactions.php">Reservations</a>
+        </li>
+      </ul>
+    </nav>
 </body>
 </html>
