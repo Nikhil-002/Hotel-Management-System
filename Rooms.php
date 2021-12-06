52869b71252a632352bd8cafbef4bb060fd2f719
Nikhil-002 <93790466+Nikhil-002@users.noreply.github.com>
2021-12-07 01:32:21 +0530

    Update Rooms.php

--- Rooms.php
+++ Rooms.php
@@ -5,32 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rooms</title>
-    <link rel="stylesheet" href="style.css">
+    lin
 </head>
 <body>
-    <table>
-       <h3>Rooms</h3>
-        <tr>
-            <th>Room No.</th>
-            <th>Room Type</th>
-            <th>Room Price</th>
-        </tr>
-        <tr>
-            <td>1</td>
-            <td>fds</td>
-            <td>45</td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>dfs</td>
-            <td>342</td>
-        </tr>
-        <tr>
-            <td>3</td>
-            <td>dg</td>
-            <td>436</td>
-        </tr>
-    </table> 
     
 </body>
 </html>
