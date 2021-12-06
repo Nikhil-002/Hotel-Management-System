538300b36e65683b373152761d8ca5c7c5a942cf
Nikhil-002 <93790466+Nikhil-002@users.noreply.github.com>
2021-12-07 01:29:45 +0530

    room

--- Rooms.php
+++ Rooms.php
@@ -5,10 +5,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rooms</title>
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Booked</h1> 
-
+    <table>
+       <h3>Rooms</h3>
+        <tr>
+            <th>Room No.</th>
+            <th>Room Type</th>
+            <th>Room Price</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>fds</td>
+            <td>45</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>dfs</td>
+            <td>342</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>dg</td>
+            <td>436</td>
+        </tr>
+    </table> 
+    
 </body>
 </html>
