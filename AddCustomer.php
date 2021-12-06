4ccee1ae4014166e4d24366d92c3c959a2dbd2a2
Pushp Jain <77234511+pushpjain3@users.noreply.github.com>
2021-12-07 01:24:27 +0530

    backend add customer done

--- AddCustomer.php
+++ AddCustomer.php
@@ -1,3 +1,46 @@
+<?php
+$insert = false;
+if(isset($_POST['cust_fname'])){
+    // Set connection variables
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+
+    // Create a database connection
+    $con = mysqli_connect($server, $username, $password);
+
+    // Check for connection success
+    if(!$con){
+        die("connection to this database failed due to" . mysqli_connect_error());
+    }
+    // echo "Success connecting to the db";
+
+    // Collect post variables
+    $cust_add = $_POST['cust_add'];
+    $cust_fname = $_POST['cust_fname'];
+    $cust_lname = $_POST['cust_lname'];
+    $cust_dob = $_POST['cust_dob'];
+    $cust_PhNo = $_POST['cust_PhNo'];
+    
+    $sql = "INSERT INTO `hotel_management`.`customer` ( `cust_add`, `cust_fname`, `cust_lname`, `cust_dob`, `cust_PhNo`) VALUES ( '$cust_add', '$cust_fname', '$cust_lname', '$cust_dob', '$cust_PhNo');";
+    // echo $sql;
+
+    // Execute the query
+    if($con->query($sql) == true){
+        // echo "Successfully inserted";
+
+        // Flag for successful insertion
+        $insert = true;
+    }
+    else{
+        echo "ERROR: $sql <br> $con->error";
+    }
+
+    // Close the database connection
+    $con->close();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,10 +55,10 @@
       <a class="navbar-brand" href="#"> HOTEL MANAGEMENT</a>
       <ul class="list-non-bullet nav-pills">
         <li class="list-item-inline">
-          <a class="link link-active" href="./">Book Room</a>
+          <a class="link " href="./">Book Room</a>
         </li>
         <li class="list-item-inline">
-          <a class="link" href="./AddCustomer.php">Add Customer</a>
+          <a class="link link-active" href="./AddCustomer.php">Add Customer</a>
         </li>
         <li class="list-item-inline">
           <a class="link" href="./Rooms.php">Rooms</a>
@@ -26,12 +69,18 @@
       </ul>
     </nav>
     <div class="container">
-      <form action="index.php">
+      <?php
+        if($insert == true){
+        echo "<p class='submitMsg'>Thanks for submitting your form. Customer Added</p>";
+        }
+    ?>
+      <form action="AddCustomer.php" method="post">
         <h1><b>Adding Customer</b></h1>
-        <input type="text" placeholder="First Name " />
-        <input type="text" placeholder="Last Name" />
-        <input type="date" name="date" id="date" placeholder="Enter your D.O.B.">
-        <input type="phone" placeholder="Enter Phone no." />
+        <input name = "cust_fname" id="cust_fname" type="text" placeholder="First Name " />
+        <input name = "cust_lname" id="cust_lname" type="text" placeholder="Last Name" />
+        <input name = "cust_dob" id="cust_dob" type="date" name="date" id="date" placeholder="Enter your D.O.B.">
+        <input name="cust_PhNo" id ="cust_PhNo" type="phone" placeholder="Enter Phone no." />
+        <textarea name="cust_add" id="decust_add" cols="30" rows="10" placeholder="Enter your address"></textarea>
         <button class="btn">Submit</button>
       </form>
     </div>
